09c185387b7b0d17df9adfd5b29d05e351d6fbb6
Abio Nataline <44293940+natalie-abbie@users.noreply.github.com>
2021-05-06 15:49:44 +0300

    add the js files

---
+++ js/tempDetect.js
@@ -0,0 +1,15 @@
+// covid temperature test
+
+let temp = 33;
+
+if (temp <=33 && temp ==35){
+    console.log("Normal temperature");
+}
+else if(temp >= 37){
+    console.log("Test for COVID");
+}
+
+else{
+    console.log("Please watch your temperature");
+}
+
